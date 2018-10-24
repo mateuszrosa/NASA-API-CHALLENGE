29bde9caf32a0a8b313014653dec7a0eb141dd11
mateuszrosa <mateusz.rosa@gmail.com>
2018-10-24 17:58:19 +0200

    projekt

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+        return gulp.src('scss/*')
+            .pipe(sourcemaps.init())
+            .pipe(sass({
+                    // outputStyle: 'compressed',
+                    outputStyle: 'expanded',
+                    sourceComments: 'map'
+                }))
+            .pipe(autoprefixer())
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('css'))
+        })
+
+gulp.task('watch', function(){
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("**/*.css").on('change', browserSync.reload);
+});
---
+++ js/app.js
@@ -0,0 +1,93 @@
+$(function() {
+
+    var sectionWelcome = $(".welcome");
+    var url = "https://api.nasa.gov/planetary/apod?";
+    var hd = "hd=true";
+    var apiKey = "api_key=GVsnIvDsUqzDjGHPX5eS1eHLsVI09H3qwq9gPw3p";
+
+    function randomingDate() {
+        var year = new Date();
+        var year1 = year.getFullYear();
+        var month = new Date();
+        var month1 = month.getMonth();
+        var day = new Date();
+        var day1 = day.getDay();
+        var randomYear = Math.floor(Math.random() * (+year1 - +1996) + 1996);
+        var randomMonth = Math.floor((Math.random() * 12) + 1);
+        var randomDay = Math.floor((Math.random() * 30) + 1);
+        if (randomYear == year1) {
+            var randomMonth = Math.floor((Math.random() * month1) + 1);
+            if (randomMonth == month1) {
+                var randomDay = Math.floor((Math.random() * day1) + 1);
+            }
+        } else {
+            var randomMonth = Math.floor((Math.random() * 12) + 1);
+        }
+        return randomYear + "-" + randomMonth + "-" + randomDay;
+    }
+    var res = randomingDate();
+    console.log(res);
+    var date = "date=" + res;
+
+    function welcome(nasa) {
+        sectionWelcome.css("background-image", "url(" + nasa.url + ')');
+        sectionWelcome.css("background-repeat", "no-repeat");
+        sectionWelcome.css("background-size", "contain");
+        sectionWelcome.css("background-position", "center");
+    }
+
+    url: "https://api.nasa.gov/planetary/apod?date=2018-10-01&hd=true&api_key=GVsnIvDsUqzDjGHPX5eS1eHLsVI09H3qwq9gPw3p"
+
+    $.ajax({
+        url: url + date + "&" + hd + "&" + apiKey
+    }).done(function(response) {
+        welcome(response);
+    }).fail(function(error) {
+        console.log(error);
+    });
+
+    var urlMars = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?";
+    var url1 = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=MAST&api_key=GVsnIvDsUqzDjGHPX5eS1eHLsVI09H3qwq9gPw3p";
+    var sol = "sol=1000";
+    var camera = "camera=NAVCAM";
+
+    $.ajax({
+        url: url1
+    }).done(function(response) {
+        insertPhotos(response);
+    }).fail(function(response) {
+        console.log(error);
+    });
+
+    function insertPhotos(nasa) {
+        var divs = $("div");
+        for (var i = 0; i <= divs.length; i++) {
+            var randomPhoto = Math.floor((Math.random() * 800) + 1);
+            $("div#" + [i]).css("background-image", "url(" + nasa.photos[randomPhoto].img_src + ')');
+            $("div#" + [i]).css("background-repeat", "no-repeat");
+            $("div#" + [i]).css("background-size", "cover");
+        }
+    };
+
+    $("span").on('click', function() {
+        $(".gallery1").toggle("hidden");
+    });
+
+    $(".first div").on("click", function() {
+        var urlMars = $(this).css("background-image");
+        $(".blank").toggle("hidden");
+        $(".blank").css("background-image", urlMars);
+        $(".blank").css("background-repeat", "no-repeat");
+        $(".blank").css("background-size", "cover");
+    })
+
+    $(".second div").on("click", function() {
+        var urlMars = $(this).css("background-image");
+        $(".blank1").toggle("hidden");
+        $(".blank1").css("background-image", urlMars);
+        $(".blank1").css("background-repeat", "no-repeat");
+        $(".blank1").css("background-size", "cover");
+    })
+
+
+})
